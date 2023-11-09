de22614bd388a68aca2fcc19c38413536c75a76a
Cmango005 <mh410676@gmail.com>
2023-11-09 22:16:56 +0600

    order delete server commit

--- index.js
+++ index.js
@@ -13,84 +13,92 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
 
-    const foodCollection = client.db('restaurantMenu').collection('menu');
-    const orderCollection = client.db('restaurantMenu').collection('order');
-    app.get('/menu', async(req, res)=>{
-        const cursor = foodCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
-    })
-   
-    app.get('/detail/:id', async(req, res)=>{
-       const id= req.params.id;
-       const query = {_id: new ObjectId(id)}
-       const options ={
-        projection: { FoodName:1 ,FoodImage:1 ,FoodCategory:1 ,Price:1 ,MadeBy:1 ,FoodOrigin:1 ,Description:1, Quantity:1, OrderNumber:1}
-       };
-       const result = await foodCollection.findOne(query,options);
-       res.send(result);
-    })
-    app.get('/order', async(req, res)=>{
-        console.log(req.query);
-        let query = {};
-        if(req.query?.email){
-            
-            query = {email: req.query.email}
-            console.log(query)
-        }
-        const cursor = orderCollection.find(query);
-        const result = await cursor.toArray();
-        res.send(result);
-    })
-    app.post('/order', async(req, res)=>{
-        const order = req.body;
-        console.log(order);
-        const result = await orderCollection.insertOne(order);
-        res.send(result)
-    })
-    app.get('/menu', async(req, res)=>{
-        if(req.query?.email){
-            
-            query = {email: req.query.email}
-            console.log(query)
-        }
-        const cursor = foodCollection.find(query);
-        const result = await cursor.toArray();
-        res.send(result);
-    })
-    app.post('/menu', async (req, res) => {
-        const newMenu = req.body;
-        console.log(newMenu);
-        const result = await foodCollection.insertOne(newMenu);
-        res.send(result)
-      })
-    
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    //await client.close();
-  }
+        const foodCollection = client.db('restaurantMenu').collection('menu');
+        const orderCollection = client.db('restaurantMenu').collection('order');
+        app.get('/menu', async (req, res) => {
+            const cursor = foodCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.get('/detail/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const options = {
+                projection: { FoodName: 1, FoodImage: 1, FoodCategory: 1, Price: 1, MadeBy: 1, FoodOrigin: 1, Description: 1, Quantity: 1, OrderNumber: 1 }
+            };
+            const result = await foodCollection.findOne(query, options);
+            res.send(result);
+        })
+        app.get('/order', async (req, res) => {
+            console.log(req.query);
+            let query = {};
+            if (req.query?.email) {
+                query = { email: req.query.email }
+                console.log(query)
+            }
+            const cursor = orderCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+        app.post('/order', async (req, res) => {
+            const order = req.body;
+            console.log(order);
+            const result = await orderCollection.insertOne(order);
+            res.send(result)
+        })
+        
+        app.delete('/order/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await orderCollection.deleteOne(query);
+            res.send(result)
+        })
+        app.get('/menu/my', async (req, res) => {
+            if (req.query?.email) {
+
+                query = { email: req.query.email }
+                console.log(query)
+            }
+            const cursor = foodCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+        app.post('/menu', async (req, res) => {
+            const newMenu = req.body;
+            console.log(newMenu);
+            const result = await foodCollection.insertOne(newMenu);
+            res.send(result)
+        })
+        
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        //await client.close();
+    }
 }
 run().catch(console.dir);
 
 
-app.get('/',(req, res)=>{
+app.get('/', (req, res) => {
     res.send('restaurant is running')
 })
-app.listen(port, ()=>{
-   console.log(`restaurant server is running ${port}`) 
+app.listen(port, () => {
+    console.log(`restaurant server is running ${port}`)
 })
