8f9051ca1da4955f87cafedebc53bf18b8ea6575
Cmango005 <mh410676@gmail.com>
2023-11-09 16:45:31 +0600

    order commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -24,12 +24,40 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const foodCollection = client.db('restaurantMenu').collection('menu');
+    const orderCollection = client.db('restaurantMenu').collection('order');
+    app.get('/menu', async(req, res)=>{
+        const cursor = foodCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+    app.get('/detail/:id', async(req, res)=>{
+       const id= req.params.id;
+       const query = {_id: new ObjectId(id)}
+       const options ={
+        projection: { FoodName:1 ,FoodImage:1 ,FoodCategory:1 ,Price:1 ,MadeBy:1 ,FoodOrigin:1 ,Description:1}
+       };
+       const result = await foodCollection.findOne(query,options);
+       res.send(result);
+    })
+    app.get('/order', async(req, res)=>{
+        const cursor = orderCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+    app.post('/order', async(req, res)=>{
+        const order = req.body;
+        console.log(order);
+        const result = await orderCollection.insertOne(order);
+        res.send(result)
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    //await client.close();
   }
 }
 run().catch(console.dir);
